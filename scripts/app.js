13b6af33837e8b194393233eab2093a8e4aa0c8f
mbd-s <michaelbsilverberg@gmail.com>
2016-03-27 14:38:35 -0700

    playing around with having the pieces inflate

--- scripts/app.js
+++ scripts/app.js
@@ -4,16 +4,32 @@ $(document).on("ready", function() {
   var $bluedot = $('#blue-dot');
 
   $(document).on("keydown", function(e) {
+    // move blue dot right with p key
     if (e.keyCode === 80) {
-      $bluedot.animate({'left': '+=20px'}, 250);
-      // move blue dot right
-    }
 
-    else if (e.keyCode === 81) {
-      $reddot.animate({'left': '+=20px'}, 250);
+      $bluedot.animate({
+
+        'width': '+=20px',
+        'height': '+=20px',
+        }, 250);
+
     }
-    // move red dot right
+    // move red dot right with q key
+    else if (e.keyCode === 81) {
+      $reddot.animate({
+
+      'width': '+=20px',
+      'height': '+=20px',
+      }, 250);
+    }
 
   });
+
+  var redWidth = $reddot.width();
+  var blueWidth = $bluedot.width();
+
+    //width of dot
+
+
 
 });
