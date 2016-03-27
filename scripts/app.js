c8a5ed6f9a87ddd50ffd06543e4aa9b597894187
mbd-s <michaelbsilverberg@gmail.com>
2016-03-27 14:54:34 -0700

    undid the blob test, added funny movements

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,25 @@
 $(document).on("ready", function() {
 
-  var $reddot = $('#red-dot');
-  var $bluedot = $('#blue-dot');
-
   $(document).on("keydown", function(e) {
+    var $reddot = $('#red-dot');
+    var $bluedot = $('#blue-dot');
     // move blue dot right with p key
     if (e.keyCode === 80) {
 
       $bluedot.animate({
-
-        'width': '+=20px',
-        'height': '+=20px',
-        }, 250);
+        'left': '+=40px',
+        // 'width': 'toggle',
+        'height': 'toggle',
+      }, 200);
 
     }
     // move red dot right with q key
     else if (e.keyCode === 81) {
       $reddot.animate({
-
-      'width': '+=20px',
-      'height': '+=20px',
-      }, 250);
+        'left': '+=40px',
+        'width': 'toggle',
+        // 'height': 'toggle',
+      }, 200);
     }
 
   });
