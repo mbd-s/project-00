fbb0f08c830a937d59c6029a07b36ca433d95182
mbd-s <michaelbsilverberg@gmail.com>
2016-03-27 15:04:33 -0700

    now with third dot

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,7 @@ $(document).on("ready", function() {
   $(document).on("keydown", function(e) {
     var $reddot = $('#red-dot');
     var $bluedot = $('#blue-dot');
+    var $yellowdot =$('#yellow-dot');
     // move blue dot right with p key
     if (e.keyCode === 80) {
 
@@ -19,6 +20,14 @@ $(document).on("ready", function() {
         'left': '+=40px',
         'width': 'toggle',
         // 'height': 'toggle',
+      }, 200);
+    }
+
+    else if (e.keyCode === 90) {
+      $yellowdot.animate({
+        'left': '+=40px',
+        'width': 'toggle',
+        'height': 'toggle',
       }, 200);
     }
 
