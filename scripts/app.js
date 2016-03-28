12d60d2eccd785d3712a74fba642cc2a32153bd2
mbd-s <michaelbsilverberg@gmail.com>
2016-03-27 20:25:15 -0700

    refinements

--- scripts/app.js
+++ scripts/app.js
@@ -5,22 +5,24 @@ $(document).on("ready", function() {
 
   $(document).on("keydown", function(event) {
 
-    var bluePos = $bluedot.offset().left;
-    var redPos = $reddot.offset().left;
-    var finishLine = ($('#track').width()-200);
+    var $bluePos = $bluedot.offset().left;
+    var $redPos = $reddot.offset().left;
+    var $finishLine = ($('#track').width());
+    var $results = $("#results-box");
 
     function checkWinner() {
-      if ((bluePos >= finishLine) || (redPos >= finishLine)) {
-        if (bluePos > redPos) {
-          $("#results-box").text("Blue wins!");
-          $("#results-box").css(color, blue);
+      $results.text("");
+      if (($bluePos >= $finishLine) || ($redPos >= $finishLine)) {
+        if ($bluePos > $redPos) {
+          $results.text("Blue wins!");
+          $results.css("color", "blue");
           }
-        else if (redPos > bluePos) {
-          $("#results-box").text("Red wins!");
-          $("#results-box").css(color, red);
+        else if ($redPos > $bluePos) {
+          $results.text("Red wins!");
+          $results.css("color", "red");
           }
         else {
-          $("#results-box").text("Tie game!");
+          $results.text("Tie game!");
         }
         $(document).unbind(event);
     }
@@ -29,7 +31,7 @@ $(document).on("ready", function() {
       // if the p key is clicked and the blue dot hasn't reached the finish line,
       // move the blue dot right
       if ((event.keyCode === 80) &&
-          (bluePos < finishLine)) {
+          ($bluePos < $finishLine)) {
             $bluedot.animate({
               'left': '+=70px',
               // 'height': 'toggle',
@@ -38,7 +40,7 @@ $(document).on("ready", function() {
       // if the q key is clicked and the red dot hasn't reached the finish line,
       // move the red dot right
       else if ((event.keyCode === 81) &&
-          (redPos < finishLine)) {
+          ($redPos < $finishLine)) {
             $reddot.animate({
               'left': '+=70px',
               // 'width': 'toggle',
